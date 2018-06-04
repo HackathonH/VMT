79e867e27a36d9e33e6849f2fa8451d7639368f3
akuraj0201 <k.anusha0201@gmail.com>
2018-06-04 16:01:34 +0530

    graph for price vs litres

---
+++ 03-saip/price.php
@@ -0,0 +1,79 @@
+<?php
+session_start();
+include("config.php");	
+$var="";
+$price=70;
+if(isset($_SESSION['name1']))
+    {
+         $var = $_SESSION['name1'];
+        
+    }
+   
+
+  //if (isset($_POST['submit'])) {
+	 // $var_name = $_POST['name1'];
+	  echo "Hello".$var;
+  
+	  //$username = $dom->getElementById('name');
+	 // echo $username;
+        //$username=echo "<script>document.getElementById("name");</script>"//$_POST['username'];
+    $litres_query = "select litres from petrol_details p,users u 
+	                  where p.user_id=u.user_id and  u.username = 'adarsh'";
+    $result = $con->query($litres_query);
+	
+	$price_array = array();
+    
+     while($row = $result->fetch_assoc() ){
+      $litres = $row['litres'];
+	  
+	  $price_array[$row['litres']]=$litres*$price;
+	 
+	
+	 }
+      
+?>
+<!DOCTYPE html>
+<html>
+<head>
+  <!-- Plotly.js -->
+  <script src="https://cdn.plot.ly/plotly-latest.min.js"></script>
+  <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+  <link rel="stylesheet" href="b1.css">
+</head>
+
+<body>
+  
+<div class="pl"; id="pli">
+
+<script>
+var dat = [];
+      var temp = [];
+	  var date = <?php echo json_encode($price_array); ?>;
+      
+	  console.log(date);
+	  litre = Object.keys(date);
+	  price = Object.keys(date).map(function(key){
+		  return date[key];
+	  });
+	  
+	  count = Object.keys(date).length;
+	  for(i = 0;i < count;i++){
+		  temp.push(litre[i].split("-")[0]);
+		  temp.push(price[i]);
+		  dat.push(temp);
+	  }
+var trace1 = {
+  x: litre,
+  y: price,
+  type: 'linechart',
+  title:'cost from refill to refill'
+};
+
+var data = [trace1];
+
+Plotly.newPlot('pli', data);
+
+</script></div>
+
+</body>
+</html>
