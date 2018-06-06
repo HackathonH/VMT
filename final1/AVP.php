e9de3781d630a228eaccd2cb0f02802c220ef12f
Saiprasad533 <36394631+Saiprasad533@users.noreply.github.com>
2018-06-06 21:16:02 +0530

    Add files via upload

---
+++ final1/AVP.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+
+
+    <form action="performance_vehicles.php">
+    <input  type="submit"  class="btn btn-info btn-lg w3-teal" style="margin-top: 30px;margin-left: 192px; " value="Click for details">
+     </form>
+     <form action="performance_vehicles_car.php">
+     <input  type="submit"  class="btn btn-info btn-lg w3-teal" style="margin-top: 30px;margin-left: 192px; " value="Click for details">
+      </form>
+</body>
+</html>
